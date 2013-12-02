415cb00c4b0341005d091d0cea88fb6749fc4980
Vladislav Koldobskiy <admin@nevergone.ru>
2013-12-02 05:13:38 +0400

    Another cleanup...

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.2.2/JDQ39/87995:user/release-keys PRIVATE_BUILD_DESC="4.2.2 Andromadus release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_vision BUILD_FINGERPRINT=htc_wwe/htc_vision/vision:4.3.1/JLS63I/87995:user/release-keys PRIVATE_BUILD_DESC="4.3.1 Andromadus test-keys"
 
 PRODUCT_NAME := cm_vision
 PRODUCT_DEVICE := vision
